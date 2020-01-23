fac167c08147139518b681fbc261d57b79c1a6cc
lee-wood <leemaewood@gmail.com>
2020-01-22 23:01:00 -0500

    added MOTD var

--- index.js
+++ index.js
@@ -12,3 +12,4 @@ server.listen(port, () => {
 //IF we want our app to use an environment variable in production, we can use an npm module that allows us to do that...".env".
 //b - this should be called as high up in the stack as possible. Also, there's no need to ser it to a varible
 //So our .env file is automatcally added to the gitignore file BUT you should always make sure that it's on the list. .env files is where you store sensitive information like auth keys and things that you do need in your code, but that you can't have hard coded and hosted in public (hello github).
+//When we deploy to Heroku, it doesn't have access to the environment variabled set in our .env file. So it's reading that it needs to get the port from `PORT` variable...but there is none, so it creates one for us. Heroku magic!
